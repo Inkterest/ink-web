54e7b3b33829ea9a5e01ebb4867dbaed1a6a8dbc
IvoRum <ivailo.rumenov@outlook.com>
2024-04-22 23:50:23 +0300

    Цреатед хеадер

--- ink-web/src/app/header/header.component.ts
+++ ink-web/src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- ink-web/src/app/landing/landing-main/landing-main.component.ts
+++ ink-web/src/app/landing/landing-main/landing-main.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-landing-main',
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './landing-main.component.html',
   styleUrl: './landing-main.component.css',
 })
