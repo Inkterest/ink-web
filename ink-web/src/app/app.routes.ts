ba6e25ae7b74b85be8342edfe14e88c410273afe
IvoRum <ivailo.rumenov@outlook.com>
2024-04-22 22:00:18 +0300

    contenplating using play java script and learn all the libraries along the way

--- ink-web/src/app/app.routes.ts
+++ ink-web/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LandingMainComponent } from './landing/landing-main/landing-main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingMainComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ ink-web/src/app/landing/landing-main/landing-main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-main',
+  templateUrl: './landing-main.component.html',
+  styleUrl: './landing-main.component.css',
+})
+export class LandingMainComponent {}
